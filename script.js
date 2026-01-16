c2600a42d610b50b46e23ab57c7c7efc86c3a325
silvers1112 <silvers1112@outlook.com>
2026-01-16 13:07:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxddG9tRkEU_wdtrX066CfYNnC7nwCpzM",
+  authDomain: "warriorcatstracker.firebaseapp.com",
+  projectId: "warriorcatstracker"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
